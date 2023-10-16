443bf79ab7e22a701c09212207b0011182636f6a
benjamin-dang <benjamin.dang@outlook.de>
2023-10-16 20:12:02 +0200

    added skills

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar';
-import Banner from './Components/Banner'
+import Banner from './Components/Banner';
+import Skills from './Components/Skills';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
+      <NavBar />
+      <Banner />
+      <Skills />
     </div>
   );
 }
